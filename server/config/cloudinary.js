d8eda5cb1d6c8f242461e019bc020f76db41bd93
Souvik Hazra <souvikhazra1901@gmail.com>
2025-08-29 11:25:47 +0530

    chnage

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,16 @@
 import { v2 as cloudinary } from "cloudinary";
 
-export default connectCloudinary = async () => {
+const connectCloudinary = async () => {
+  try {
     cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET,
-    })
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
+    });
+    console.log("✅ Cloudinary Connected Successfully");
+  } catch (error) {
+    console.error("❌ Cloudinary Connection Failed", error);
+  }
 };
+
+export default connectCloudinary;
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,4 +1,4 @@
-import Product from "../models/product.models";
+import Product from "../models/product.models.js";
 import { v2 as cloudinary } from 'cloudinary';
 
 export const addProduct = async (req, res) => {
